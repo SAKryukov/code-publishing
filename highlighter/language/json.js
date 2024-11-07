f2ef31606c6ceb6ed07fefc00d942921853d418a
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2024-11-07 17:26:07 -0500

    Highlighter: re-implemented JSON

--- highlighter/language/json.js
+++ highlighter/language/json.js
@@ -1,8 +1,20 @@
 /**
  * JSON patterns
  *
- * @author Nijiko Yonskai
- * @author Craig Campbell
+ * @author Sergey A Kryukov
  */
 RuleSet.extend('json', [
+    {
+        name: 'string',
+        pattern: /".*?"/g
+    },
+    {
+        name: 'operator',
+        pattern: /[\[|\]|\{|}]/g
+    },
+    {
+        name: 'property',
+        pattern: /\".*?"\s*?:/g
+    },
+
 ]);
