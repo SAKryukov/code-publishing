1ecb52a12fe794a4c4c9ebded9902142a51dad49
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2024-11-09 19:50:42 -0500

    Referential integrity validation: introduced

---
+++ referential-integrity-validation/validator.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * Syntax highlighter
+ *
+ * @author Sergey A Kryukov, https://www.SAKryukov.org
+ To detect invalid anchor targets, add to the HTML document:
+ *     <script src="prepare/validator.js"></script>
+*/
+
+window.onload = () => {
+
+    const errors = [];
+    const external = [];
+    const anchors = document.getElementsByTagName("a");
+    for (let anchor of anchors) {
+        if (!anchor.textContent) continue;
+        if (!anchor.href) continue;
+        if (anchor.protocol == "file:") {
+            if (!anchor.hash) continue;
+            element = document.getElementById(anchor.hash.replace("#", ""));
+            if (!element)
+                errors.push(anchor);    
+        } else
+            external.push(anchor);
+    } //loop
+
+    if (errors.length > 0) {
+        document.writeln(`<h1>Errors: invalid anchor hrefs:</h1>`);
+        for(let anchor of errors)
+            document.writeln(`<h3> ${anchor.hash}<dd>Text: "${anchor.textContent}"</dd></h3>`);
+    } //if
+
+    if (external.length > 0) {
+        document.writeln(`<h1>External anchors:</h1>`);
+        for(let anchor of external)
+            document.writeln(`<a href="${anchor.href}">${anchor.textContent}</a><br/>`);
+    } //if
+
+} //window.onload
